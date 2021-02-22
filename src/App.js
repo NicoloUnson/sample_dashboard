b388ff444db092426cce43178d44ff9a568be585
drei-kun <66506208+drei-kun@users.noreply.github.com>
2021-02-22 18:34:36 +0800

    feat: added tooltips for the papers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Container, Icon, IconButton, Typography } from '@material-ui/core';
 import SettingsIcon from '@material-ui/icons/Settings';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from "@material-ui/core/Container";
+import Icon from "@material-ui/core/Icon";
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Tooltip from '@material-ui/core/Tooltip';
 
-import Odometer from './Odometer'
-import Utilization from './Utilization';
-import Table from './BasicTable';
-import CompareTable from './CompareTable'
+import Odometer from './odometer'
+import Utilization from './utilization';
+import Table from './basictable';
+import CompareTable from './comparetable';
+import Averages from "./averages";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,12 +36,63 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
-    border: `3px solid #FFD700`
+    border: `3px solid #FFD700`,
+    backgroundColor: "#EAF8F3",
   },
   container: {
-    height: '100vh',
-    maxWidth: '85vw'
-  }
+    height: '90vh',
+    maxWidth: '85vw',
+    marginTop: "15px"
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+  },
 }));
 
 const createData = (x, y, z) => {
@@ -71,24 +139,101 @@ const compareTableData = [
   createTableData('MNO-123', '68', 'JJJ-556', '90')
 ]
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
+
 export default function App() {
-  const [lastUpdated,setLastUpdated] = useState('today')
+  const [lastUpdated, setLastUpdated] = useState("Today");
   const classes = useStyles();
   const rows = efficiencyData
   return (
     <div className={classes.root}>
-
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            MASSIVE! Dashboard
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
       <Container className={classes.container}>
         <Grid container spacing={5}>
           <Grid border item lg={5} xs={12}>
             <Paper elevation={7} className={classes.paper}>
-              <Odometer />
+              <Utilization />
               <Grid container>
                 <Grid item xs={10} />
                 <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Utilization is number of garaged units</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -98,13 +243,23 @@ export default function App() {
           </Grid>
           <Grid item lg={5} xs={12}>
             <Paper elevation={7} className={classes.paper}>
-              <Utilization />
+              <Odometer />
               <Grid container>
                 <Grid item xs={10} />
                 <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Odometer is the average kilometers travelled.</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -114,16 +269,23 @@ export default function App() {
           </Grid>
           <Grid item lg={2} xs={12}>
             <Paper elevation={7} className={classes.paper}>
-              <Typography variant="h3">
-                Odometer
-              </Typography>
-              <Grid container direction="row-reverse">
-                <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
-                </Grid>
-                <Grid item xs={2}>
+              <Averages />
+              <Grid container>
+                <Grid item xs={8} />
+                <Grid item xs={4}>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Averages show the average value of these values.</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -135,11 +297,21 @@ export default function App() {
             <Paper elevation={7} className={classes.paper}>
               <Table rows={rows} colName={efficiencyCol} />
               <Grid container>
-                <Grid item xs={10} />
-                <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
+                <Grid item xs={8} />
+                <Grid item xs={4}>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Top or Bottom Five of Efficiency data.</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -151,11 +323,21 @@ export default function App() {
             <Paper elevation={7} className={classes.paper}>
               <Table rows={engineRunTime} colName={engineRunTimeCol} />
               <Grid container>
-                <Grid item xs={10} />
-                <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
+                <Grid item xs={8} />
+                <Grid item xs={4}>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Top or Bottom Five of Engine Runtime data.</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -168,11 +350,21 @@ export default function App() {
               <CompareTable rows={compareTableData} />
 
               <Grid container>
-                <Grid item xs={10} />
-                <Grid item xs={2}>
-                  <IconButton>
-                    <HelpOutlineIcon />
-                  </IconButton>
+                <Grid item xs={8} />
+                <Grid item xs={4}>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <i>Comparison table for Engine Runtime.</i>
+                        <hr class="solid" />
+                        <i>Last Updated: { lastUpdated }</i>
+                      </React.Fragment>
+                    }
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon />
+                    </IconButton>
+                  </HtmlTooltip>
                   <IconButton>
                     <SettingsIcon />
                   </IconButton>
@@ -181,14 +373,17 @@ export default function App() {
             </Paper>
           </Grid>
         </Grid>
-        <Grid container direction="row-reverse">
-          <Grid item xs={2}>
-            <Typography>
-              Last updated: {lastUpdated}
-            </Typography>
-          </Grid>
-        </Grid>
       </Container>
     </div>
   );
 }
+
+
+
+// Color Scheme:
+// #ADD4DC
+// #EAF8F3
+// #37858D
+// #1F5068
+// #F8FBFA
+// #DBAC3D
